491b17beb61b94801a0eb27881a1b9d046e43e4f
Carlos de la Fuente <carlos.delafuente@erni-espana.es>
2020-03-04 16:19:23 +0100

    ADD mini-max-sum challenge: init

---
+++ basics/src/basics/hackerrank/algorithms/MiniMaxSum.java
@@ -0,0 +1,29 @@
+package basics.hackerrank.algorithms;
+
+import java.util.Scanner;
+
+public class MiniMaxSum {
+
+	// Complete the miniMaxSum function below.
+	static void miniMaxSum(int[] arr) {
+
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+
+		String[] arrItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < 5; i++) {
+			int arrItem = Integer.parseInt(arrItems[i]);
+			arr[i] = arrItem;
+		}
+
+		miniMaxSum(arr);
+
+		scanner.close();
+	}
+}
