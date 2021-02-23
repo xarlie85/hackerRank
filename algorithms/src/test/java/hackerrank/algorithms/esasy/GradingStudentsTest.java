e7f389fe5f0c2230baf759ad3b8e5f842046ca75
Carlos de la Fuente <carlos.delafuente@erni-espana.es>
2021-02-23 11:30:52 +0100

    TDD-ing grading students

--- algorithms/src/test/java/hackerrank/algorithms/esasy/GradingStudentsTest.java
+++ algorithms/src/test/java/hackerrank/algorithms/esasy/GradingStudentsTest.java
@@ -1,11 +1,40 @@
 package hackerrank.algorithms.esasy;
 
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class GradingStudentsTest {
 
-    @Test
-    void name() {
+    public static final GradingStudents cut = new GradingStudents();
 
+    // TDD 0
+    @Test
+    void gradingStudents_returnsAListOfIntegers() {
+        List<Integer> grades = GradingStudents.gradingStudents(Collections.EMPTY_LIST);
+        assertThat(grades, notNullValue(List.class));
     }
+
+    // TDD 1
+    @Test
+    void gradingStudents_returnSameNumberOfElements() {
+        List<Integer> grades = GradingStudents.gradingStudents(Arrays.asList(25, 33, 38, 42, 34, 85, 100, 99, 98, 97));
+        List<Integer> expectedGrades = Arrays.asList(25, 33, 40, 42, 34, 85, 100, 100, 100, 97);
+        assertThat(grades.size(), is(expectedGrades.size()));
+    }
+
+    // TDD
+    @Test
+    void gradingStudents_returnsGradesRounded() {
+        List<Integer> grades = GradingStudents.gradingStudents(Arrays.asList(25, 33, 38, 42, 34, 85, 100, 99, 98, 97));
+        List<Integer> expectedGrades = Arrays.asList(25, 33, 40, 42, 34, 85, 100, 100, 100, 97);
+        assertArrayEquals(expectedGrades.toArray(),grades.toArray());
+    }
+
 }
